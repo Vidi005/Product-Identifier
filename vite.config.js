b0b5eff48d8d865226b4dd33b5d5dcb93cf0ce8e
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-01-04 21:28:45 +0700

    Fixing Vite Config

--- vite.config.js
+++ vite.config.js
@@ -24,13 +24,13 @@ export default defineConfig({
             type: 'image/x-icon'
           },
           {
-            src: 'pi-logo-192x192.png',
+            src: 'images/pi-logo-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'pi-logo-512x512.png',
+            src: 'images/pi-logo-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
