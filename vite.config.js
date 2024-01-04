7cc4e951e1274d0eaff0a383e6310a6ef613162f
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2024-01-04 21:20:11 +0700

    Adding Icons and PWA Support

--- vite.config.js
+++ vite.config.js
@@ -6,36 +6,34 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/',
   server: { port: 3000 },
-  optimizeDeps: {
-    include: ['tesseract.js/dist/tesseract.min.js'],
-  },
   plugins: [
     legacy({
-      targets: ['defaults', 'not IE 11'],
+      targets: ['defaults', 'not IE 11']
     }),
     VitePWA({
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'Product Identifier',
         short_name: 'Product Identifier',
-        description: 'Product Identifier',
+        description: 'Identifying Product Names and Vendors using Text Recognition and Barcode Scanner',
         theme_color: '#ffffff',
         icons: [
           {
             src: 'favicon.ico',
             sizes: '64x64 32x32 24x24 16x16',
-            type: 'image/png',
+            type: 'image/x-icon'
           },
           {
-            src: 'pwa-192x192.png',
+            src: 'pi-logo-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pi-logo-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
+            purpose: 'any maskable'
           }
         ],
         display: 'standalone'
